23599629950e669cfc8ce276af14dd26f67f17e9
katini123 <31271984+katini123@users.noreply.github.com>
2019-08-01 23:35:54 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,12 @@
+/*Addition of two numbers*/
+#include<stdio.h>
+int main()
+{
+ int a,b,sum;
+ printf("Input the value of a and b:");
+ scanf("%d %d",&a,&b);
+ sum = a+b;
+ printf("The addition of two number is %d",sum);
+ getch();
+ return 0;
+}
